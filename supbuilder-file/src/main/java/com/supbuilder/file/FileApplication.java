0c91a95ed95ce3d6b632416888d0decb99e1914e
jiaokeqing <13583243451@163.com>
2022-12-12 23:28:31 +0800

    降低springcloud版本，为文件服务配置ribbon

--- supbuilder-file/src/main/java/com/supbuilder/file/FileApplication.java
+++ supbuilder-file/src/main/java/com/supbuilder/file/FileApplication.java
@@ -1,13 +1,38 @@
 package com.supbuilder.file;
 
 
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FileApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class);
     }
+
+    /**
+     * 创建RestTemplate注入容器并开启负载均衡
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    /**
+     * 修改负载均衡策略
+     * @return
+     */
+    @Bean
+    public IRule bestAvailableRule(){
+        return new BestAvailableRule();
+    }
+
+
 }
