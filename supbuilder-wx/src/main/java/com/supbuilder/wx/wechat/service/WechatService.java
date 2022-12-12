03b3c0169a2d84f92b8c0893cc1cbf26f5dbbb8b
jiaokeqing <13583243451@163.com>
2022-12-12 20:30:59 +0800

    删除微信和核心模块，后续将单机架构改为微服务架构

--- supbuilder-wx/src/main/java/com/supbuilder/wx/wechat/service/WechatService.java
+++
@@ -1,9 +0,0 @@
-package com.supbuilder.wx.wechat.service;
-
-import com.github.binarywang.wxpay.exception.WxPayException;
-import com.supbuilder.common.vo.Result;
-
-public interface WechatService {
-
-    Result<Object> pay(String openId, int price, String ip) throws WxPayException;
-}
