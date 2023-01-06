45e3280ce2f42096bf1ed88d278e19e8ea101f5d
jiaokeqing <13583243451@163.com>
2023-01-06 17:30:35 +0800

    文件处理服务依赖配置

--- supbuilder-file/supbuilder-file-service/src/main/java/com/supbuilder/file/FileServiceApplication.java
+++ supbuilder-file/supbuilder-file-service/src/main/java/com/supbuilder/file/FileServiceApplication.java
@@ -1,9 +1,9 @@
 package com.supbuilder.file;
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class FileServiceApplication {
     public static void main(String[] args) {
